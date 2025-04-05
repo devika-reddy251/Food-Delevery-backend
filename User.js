492cfcf425ea290bac7047488cf216ebd12f4e90
devika-reddy251 <devikareddy4002@gmail.com>
2025-04-05 21:32:16 +0530

    Update User.js

--- User.js
+++ User.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 
-const UserSchema = mongoose.Schema({
-    user: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true }
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
 
 module.exports = mongoose.model('User', UserSchema);
